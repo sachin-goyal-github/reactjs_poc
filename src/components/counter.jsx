7e8756dbd9eefeb30b46f2f57cb7d8702689fdd0
Sachin Goyal <sachin@fanduel.com>
2019-02-03 22:52:44 +0000

    move state out

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,18 +2,20 @@ import React, {Component} from 'react';
 
 export default class Counter extends Component {
     render() {
+        // it's called object destructuring
+        const {onIncrement, onDecrement, onDelete, totalCounters, counter} = this.props;
         return (
             <div className='m-4'>
-                <span className='badge badge-info m-1'>{this.props.counter.count}</span>
-                <button onClick={this.props.onIncrement} className='btn btn-info m-1'>
+                <span className='badge badge-info m-1'>{counter.count}</span>
+                <button onClick={onIncrement} className='btn btn-info m-1'>
                     +
                 </button>
-                <button onClick={this.props.onDecrement} className='btn btn-info m-1'
-                        disabled={this.props.counter.count === 0}>
+                <button onClick={onDecrement} className='btn btn-info m-1'
+                        disabled={counter.count === 0}>
                     -
                 </button>
-                <button onClick={this.props.onDelete}
-                        disabled={this.props.totalCounters === 1}
+                <button onClick={onDelete}
+                        disabled={totalCounters === 1}
                         className='btn btn-danger m-1'>
                     Delete
                 </button>
